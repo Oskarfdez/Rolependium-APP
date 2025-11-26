4ec985057e9e87f8c9cc104faec4a648f289ab0c
oscar <oscarfercay@gmail.com>
2025-11-26 21:40:39 +0100

    Eliminado importaciones sin usar

--- app/src/main/java/com/example/login/presentation/menu/MenuScreen.kt
+++ app/src/main/java/com/example/login/presentation/menu/MenuScreen.kt
@@ -15,15 +15,11 @@ import androidx.compose.ui.draw.clip
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.res.painterResource
-import com.example.login.R
 import com.example.login.presentation.components.BottomBar
 import com.example.login.presentation.components.TopBar
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.LazyColumn
-import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.shape.RoundedCornerShape
-import androidx.compose.foundation.verticalScroll
 import androidx.compose.material3.Card
 import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.MaterialTheme
